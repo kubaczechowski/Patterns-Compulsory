14106da4118ba1b00a110caf566bf9e17e2eafb0
kubaczechowski <jcobczechowski@gmail.com>
2021-03-16 11:56:25 +0100

    business entity created

---
+++ src/sample/BE/Message.java
@@ -0,0 +1,13 @@
+package sample.BE;
+
+/**
+ * @author Kuba
+ * @date 3/16/2021 11:18 AM
+ */
+public class Message {
+    String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+}
