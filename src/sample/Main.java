9bd8916da5e18e05b3b51260dfeabbb2929f6a7f
kubaczechowski <jcobczechowski@gmail.com>
2021-03-22 16:37:19 +0100

    added icon

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +12,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("GUI/View/sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Message app");
         primaryStage.setScene(new Scene(root));
+        primaryStage.getIcons().add(new Image("/sample/GUI/images/icon.png"));
         primaryStage.show();
     }
 
